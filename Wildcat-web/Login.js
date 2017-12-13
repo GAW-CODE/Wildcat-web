bd661b1ee6503f961206c06994f79e2dd0bb59bb
Keith Kwong <chubbster7134@gmail.com>
2017-12-12 16:31:44 -0800

    added javascript to login

---
+++ Wildcat-web/Login.js
@@ -0,0 +1,29 @@
+
+  var config = {
+    apiKey: "AIzaSyAOCBR_8j5Uf59kPrJeNKmyhm5kkdVUJWo",
+    authDomain: "gaw-wildcat-app.firebaseapp.com",
+    databaseURL: "https://gaw-wildcat-app.firebaseio.com",
+    projectId: "gaw-wildcat-app",
+    storageBucket: "gaw-wildcat-app.appspot.com",
+    messagingSenderId: "467203906935"
+  };
+
+  firebase.initializeApp(config);
+
+
+  const txtEmail = document.getElementById('txtEmail');
+  const txtPassword = document.getElementById('txtPassword');
+  const btnLogin = document.getElementById('btnLogin');
+  const btnSignUp = document.getElementById('btnSignUp');
+
+  btnLogin.addEventListener('click', e => {
+  	const email = txtEmail.value;
+  	const pass = txtPassword.value;
+  	const auth = firebase.auth();
+  	const promise = auth.signInWithEmailAndPassword(email, pass);
+    promise.then(function(v){
+      window.location.href = "admin.html";
+    });
+  	promise.catch(e => console.log(e.message));
+  });
+
