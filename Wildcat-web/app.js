653c6dff2659c4ada11d9c636aa137c985192d4a
Darien Tsai <smartdragondt@gmail.com>
2017-12-28 11:40:47 -0800

    added wits logos

--- Wildcat-web/app.js
+++ Wildcat-web/app.js
@@ -8,10 +8,26 @@
 var desc = document.getElementsByClassName('reqDesc');
 var req = document.getElementsByClassName('req');
 
+var temp = 0;
 for(var i = 0; i < req.length; i++){
-    req[i].addEventListener('click', hideReq(i));
+    //req[i].addEventListener("click" , description(req[i]));
+    temp = req[i];
+    req[i].addEventListener("click" , function(temp){
+        console.log(temp);
+    });
 }
 
-function hideReq(i){
-    desc[i].classList.toggle("descHide");
- }
+function description(selection){
+    var check = true;
+    console.log(selection);
+    if(check){
+        console.log("true");
+        selection.childNodes[1].class = "descHide";
+        check = false;
+    }
+    else{
+        console.log("false");
+        selection.childNodes[1].class = "reqDesc";
+        check = true;
+    }
+}
