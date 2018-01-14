af44d71a661328655076c10a0492635d379ca64f
Darien Tsai <smartdragondt@gmail.com>
2018-01-14 12:34:57 -0800

    workingo n directory

--- public/js/directory.js
+++ public/js/directory.js
@@ -4,18 +4,23 @@
 var home = document.getElementById('directory');
 var academics = document.getElementById('academicDirectory');
 var sports = document.getElementById('sportsDirectory');
-var SFall = document.getElementById('fallDirecory');
+var SFall = document.getElementById('fallDirectory');
 var SWinter = document.getElementById('winterDirectory');
-var SSpring = document.getElementById('springDirecotry');
+var SSpring = document.getElementById('springDirectory');
 var groups = document.getElementById('groupsContact');
 var math = document.getElementById('mathDirectory');
 var hist = document.getElementById('socialStudiesDirectory');
-var science = document.getElementById('scienceDirectory');
+var sci = document.getElementById('scienceDirectory');
 var eng = document.getElementById('englishDirectory');
 var pe = document.getElementById('peDirectory');
 var wits = document.getElementById('witsDirectory');
-var vapa = documnet.getElementById('vapaDirectory');
+var vapa = document.getElementById('vapaDirectory');
 var lang = document.getElementById('modernLanguagesDirectory');
 
-/*test*/ 
-console.log();
+/*home direcotry*/
+var schoolDir = document.getElementById('school');
+var groupsDir = document.getElementById('groups');
+var sportsDir = document.getElementById('sports');
+var clubsDir = document.getElementById('clubs');
+
+schoolDir.addEventListener('click', function(){});
