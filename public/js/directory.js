2e7b131f2719786a9d7c8797406620600bfca7bf
Darien Tsai <smartdragondt@gmail.com>
2018-01-14 09:10:25 -0800

    reverted announce

---
+++ public/js/directory.js
@@ -0,0 +1,21 @@
+/*interface js*/
+
+/*them pages*/
+var home = document.getElementById('directory');
+var academics = document.getElementById('academicDirectory');
+var sports = document.getElementById('sportsDirectory');
+var SFall = document.getElementById('fallDirecory');
+var SWinter = document.getElementById('winterDirectory');
+var SSpring = document.getElementById('springDirecotry');
+var groups = document.getElementById('groupsContact');
+var math = document.getElementById('mathDirectory');
+var hist = document.getElementById('socialStudiesDirectory');
+var science = document.getElementById('scienceDirectory');
+var eng = document.getElementById('englishDirectory');
+var pe = document.getElementById('peDirectory');
+var wits = document.getElementById('witsDirectory');
+var vapa = documnet.getElementById('vapaDirectory');
+var lang = document.getElementById('modernLanguagesDirectory');
+
+/*test*/ 
+console.log();
