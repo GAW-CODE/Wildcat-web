57c42809106593c48b98c4fff85ba369c1faa00c
Kyle Dang <justsogaming253@gmail.com>
2018-02-10 21:34:13 -0800

    Moved calendar javascript files into existing calendar file

--- public/js/request-date.js
+++ public/js/request-date.js
@@ -1,8 +1,25 @@
+var today = new Date();
+var dd = today.getDate()+1;
+var mm = today.getMonth()+1; //January is 0!
+var yyyy = today.getFullYear();
+
+if(dd<10) {
+    dd = '0'+dd
+}
+
+if(mm<10) {
+    mm = '0'+mm
+}
+
+today = mm + '/' + dd + '/' + yyyy;
+
+
 angular.module("cuppaDatepicker",["cuppaDatepickerDirective"])
 .controller("cuppaDatepickerController",function($scope){
-        $scope.myDate = "04/18/1990 12:15 AM";
-        $scope.myDate2 = "04/18/1990 12:15 AM";
+        $scope.myDate = today;
         $scope.onDateSelect = function(dateVal){
             //alert(dateVal);
         }
 });
+var selectedDate=myDate;
+document.getElementById("yee").innerHTML = selectedDate;
