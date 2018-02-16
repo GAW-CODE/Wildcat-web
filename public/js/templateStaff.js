0f21d65b12f55eac2b282c189c955bc67febf8c7
Kalvin Chang <kalvin1204@gmail.com>
2018-02-16 01:07:14 -0800

    got name to transfer from appDirectory.html to card template

--- public/js/templateStaff.js
+++ public/js/templateStaff.js
@@ -1,5 +1,15 @@
 //recognize WHO user clicks on - get the name of the person whose contact card user clicked
+let name;
+
+//https://stackoverflow.com/questions/17502071/transfer-data-from-one-html-file-to-another
+window.onload = function() {
+    let url = document.location.href;
+		let params = url.split('?')[1].split('&')[0];
+		name = decodeURIComponent(params.substring(params.indexOf('=') + 1));
+}
+
 //perform a search query IN THE DATABASE under the '/directory' folder
+
 //find the key of the desired person
 //READ data from Database - using key
 //display it with DOM
@@ -11,7 +21,7 @@ FIREBASE_DATABASE.ref('/directory/academics/math').once('value')
 				//code that handles the snapshot
 				//snapshot.val() is the actual, usable data (parsed into JSON)
 			let div = document.createElement('div');
-			let domString = ``;
+			let domString = ``; //use template literals here
 			div.innerHTML = domString;
-			selectedElement.appendChild(div.firstChild);
+			//selectedElement.appendChild(div.firstChild);
 	});
