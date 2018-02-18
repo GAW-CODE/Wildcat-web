4c3796b63c0e7e952fef5bf82ed2d9b88722c767
Kalvin Chang <kalvin1204@gmail.com>
2018-02-17 18:04:01 -0800

    fixing up login and signup

--- public/js/login.js
+++ public/js/login.js
@@ -10,19 +10,17 @@ btnLogin.addEventListener('click', e => {
   const PASS = txtPassword.value;
   const EMAIL = txtEmail.value;
   const FIREBASE_AUTH = firebase.auth();
-  const PROMISE = FIREBASE_AUTH.signInWithEmailAndPassword(EMAIL, PASS);
-  const UID = FIREBASE_AUTH.currentUser.uid;
-    //First check the username and password
-    //Second read the type from the user
-    //Third move them to the webpage them need to go
-  firebase.database().ref('/users/' + UID).once('value')
-      .then((snapshot) => {
-          console.log(snapshot.val().type);
-          //student go to annouce
-          //campus org, guidance, ASB, Athletics, Fundraiser  go to orginization
-          //Admin go to admin
-          
-          PROMISE.then(function (v) {
+  const PROMISE = FIREBASE_AUTH.signInWithEmailAndPassword(EMAIL, PASS)
+    .then(function(user) {
+        //First check the username and password
+        //Second read the type from the user
+        //Third move them to the webpage them need to go
+      firebase.database().ref('/users/' + user.uid).once('value')
+          .then((snapshot) => {
+              console.log(snapshot.val().type);
+              //student go to annouce
+              //campus org, guidance, ASB, Athletics, Fundraiser  go to orginization
+              //Admin go to admin
               if (snapshot.val().type == 'Student') {
                   window.location.href = 'announce.html'
               } else if (snapshot.val().type == 'Admin') {
@@ -31,12 +29,8 @@ btnLogin.addEventListener('click', e => {
                   window.location.href = 'organization.html'
               }
           });
-      });
-  /*PROMISE.then(function(v){
-    window.location.href = "admin.html";
-    //else redirect to request.html (and eventually a separate interface for campus orgs / teachers)
-  });
-    */
-  PROMISE.catch(e => console.log(e.message));
-  document.getElementById('wrong').style.display = "block";
+    }).catch(e => {
+        console.log(e.message);
+        document.getElementById('wrong').style.display = "block";
+    });
 });
