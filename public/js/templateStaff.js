ea58e8705d98522a911bf87ad88509284b61963d
Kalvin Chang <kalvin1204@gmail.com>
2018-02-19 09:46:33 -0800

    another attempt at making app standalone app on a phone; directory is now able to READ data of STAFF stored in the database

--- public/js/templateStaff.js
+++ public/js/templateStaff.js
@@ -3,49 +3,28 @@ let name;
 let currentDepartment;
 let key;
 //https://stackoverflow.com/questions/17502071/transfer-data-from-one-html-file-to-another
-window.onload = function() {
-    let url = document.location.href;
-	let params = url.split('?')[1].split('&')[0];
-	name = decodeURIComponent(params.substring(params.indexOf('=') + 1));
-	currentDepartment = name.split(' .')[1];
-	name = name.split(' .')[0];
-}
 
-//perform a search query IN THE DATABASE under the 
-//find the key of the desired person
-//READ data from Database - using key
-//display it with DOM
-const FIREBASE_DATABASE = firebase.database();//access the database
+let url = document.location.href;
+let params = url.split('?')[1].split('&')[0];
+name = decodeURIComponent(params.substring(params.indexOf('=') + 1));
+currentDepartment = name.split(' .')[1];
+name = name.split(' .')[0];
 
-FIREBASE_DATABASE.ref('/directory/academics/' + currentDepartment).once('value')
-	.then((snapshot) => {
-		console.log(name);
-		console.log(currentDepartment);
-		FIREBASE_DATABASE.ref('/directory/academics/' + currentDepartment).orderByChild("Name").equalTo(name).on("child_added", function(snapshot) {
-  			key = snapshot.key;
-		});
+//perform a search query IN THE DATABASE under the '/directory' folder
+const FIREBASE_DATABASE = firebase.database(); //access the database
 
-		FIREBASE_DATABASE.ref('/directory/academics/' + currentDepartment + '/' + key).once('value')
-			.then((snapshot) => {
-				console.log("/directory/academics/" + currentDepartment + "/" + key);
-				console.log(snapshot.val());
-				console.log(key);
-				let div = document.createElement("div");
-    			let information = document.createTextNode(snapshot.val().Name);
-   				div.appendChild(information);
-    			document.getElementById("roomNum").appendChild(div);
-    		});
-		//let div = document.createElement('div');
-		//let domString = ``; //use template literals here
-		//div.innerHTML = domString;
-		////selectedElement.appendChild(div.firstChild);
+//make sure currentDepartment value is obtained first
+//snapshot contains the ENTIRE data of the matching person (key not needed)
+FIREBASE_DATABASE.ref('/directory/academics/' + currentDepartment).orderByChild("Name").equalTo(name).once('value')
+  .then((snapshot) => {
+    console.log(snapshot.val());
+    //READ data from Database
+    //display it with DOM
 
-	});
-
- //     var key = childSnapshot.key;
-//      var childData = childSnapshot.val();
-//
-  //    var name_val = childSnapshot.val().Name;
-    //  var id_val = childSnapshot.val().AssignedID;
-      //document.getElementById("name").innerHTML = name_val;
-     // document.getElementById("id").innerHTML = id_val;
+    let div = document.createElement('div');
+    let information = document.createTextNode(snapshot.val().Name);
+    div.appendChild(information);
+    document.getElementById("roomNum").appendChild(div.firstChild);
+    //you don't necessarily have to append a new element in your case, Jeffrey
+    //you should just change the text of pre-existing elements
+  });
