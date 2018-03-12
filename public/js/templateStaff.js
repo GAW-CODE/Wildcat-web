0932327dc54f5613a01652bc985fbb786421baff
jeffreyL02 <jeffreyliu02@gmail.com>
2018-03-12 07:44:42 -0700

    made an array with all teachers and organizations

--- public/js/templateStaff.js
+++ public/js/templateStaff.js
@@ -1,8 +1,6 @@
-//recognize WHO user clicks on - get the name of the person whose contact card user clicked
 let name;
 let currentDepartment;
 let key;
-//https://stackoverflow.com/questions/17502071/transfer-data-from-one-html-file-to-another
 
 let url = document.location.href;
 let params = url.split('?')[1].split('&')[0];
@@ -10,14 +8,9 @@ name = decodeURIComponent(params.substring(params.indexOf('=') + 1));
 currentDepartment = name.split(' .')[1];
 name = name.split(' .')[0];
 
-//perform a search query IN THE DATABASE under the '/directory' folder
 const FIREBASE_DATABASE = firebase.database(); //access the database
 
-//make sure currentDepartment value is obtained first
-//snapshot contains the ENTIRE data of the matching person (key not needed)
 FIREBASE_DATABASE.ref('/directory/academics/' + currentDepartment).orderByChild("Name").equalTo(name).on('value', function(snapshot) {
-    //READ data from Database
-    //display data with DOM
     let key;
     snapshot.forEach(function(data) { //assumptiin: only one teacher with a certain name exists at Wilson
         key = data.key;
