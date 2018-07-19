7fe7b4862d183f370c3bbbe4b6ff839e80d467c7
Shieu-Hong Lin <shieuhongl@ad.biola.edu>
2018-07-19 14:08:44 -0700

    countdown characters

--- public/js/0rg.js
+++ public/js/0rg.js
@@ -1,6 +1,38 @@
-function count_up(obj){
+/*function count_up(obj){
   document.getElementById('count1').innerHTML=obj.value.length;
   if(obj.value.length==500){
     alert("You have reached the character limit.");
   }
 }
+
+function count2_up(obj){
+  document.getElementById('count2').innerHTML=obj.value.length;
+  if(obj.value.length==500){
+    alert("You have reached the character limit.");
+  }
+}
+*/
+function count_up(obj){
+  var element=document.getElementById('count1');
+  element.innerHTML= 500-obj.value.length;
+  if (500 - obj.value.length==0){
+                element.style.color = 'red';
+                alert("You have reached the character limit.");
+
+            } else {
+                element.style.color = 'grey';
+            }
+
+        }
+function count2_up(obj){
+   var element=document.getElementById('count2');
+   element.innerHTML= 500-obj.value.length;
+    if (500 - obj.value.length==0){
+                  element.style.color = 'red';
+                  alert("You have reached the character limit.");
+
+                  } else {
+                  element.style.color = 'grey';
+                  }
+
+                }
